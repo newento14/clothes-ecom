ad66d570fa9bc639c5e371e2a5ce49b3985133fe
Yuriy <yurii.choboda@gmail.com>
2023-12-03 09:50:34 +0200

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
-  const PORT = 8899;
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
